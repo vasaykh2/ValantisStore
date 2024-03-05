ad9cbf164cf83fe350d055c7e27c52ab61d1912b
Vadim Savinov <vasaykh@yandex.ru>
2024-03-06 01:35:33 +0500

    feat: начальная верстка и логика

---
+++ src/api.js
@@ -0,0 +1,117 @@
+// src/api.js
+import md5 from "md5";
+
+const API_URL = "https://api.valantis.store:41000/"; //  URL для api
+
+const authenticate = () => {
+  const password = "Valantis"; // Наш пароль
+  const timestamp = new Date().toISOString().split("T")[0].replace(/-/g, "");
+  const authString = `${password}_${timestamp}`;
+  const xAuth = md5(authString);
+
+  // console.log(xAuth);
+
+  return {
+    headers: {
+      "X-Auth": xAuth,
+      "Content-Type": "application/json",
+    },
+  };
+};
+
+const api = {
+  getAllIds: async () => {
+    try {
+      const response = await fetch(API_URL, {
+        method: "POST",
+        ...authenticate(),
+        body: JSON.stringify({
+          action: "get_ids",
+        }),
+      });
+      const data = await response.json();
+      // console.log(data.result);
+      return data.result;
+    } catch (error) {
+      console.error("Error:", error);
+      return [];
+    }
+  },
+
+  getIds: async (offset, limit) => {
+    try {
+      const response = await fetch(API_URL, {
+        method: "POST",
+        ...authenticate(),
+        body: JSON.stringify({
+          action: "get_ids",
+          params: { offset, limit },
+        }),
+      });
+      const data = await response.json();
+      // console.log(data.result);
+      return data.result;
+    } catch (error) {
+      console.error("Error:", error);
+      return [];
+    }
+  },
+
+  getItems: async (ids) => {
+    try {
+      const response = await fetch(API_URL, {
+        method: "POST",
+        ...authenticate(),
+        body: JSON.stringify({
+          action: "get_items",
+          params: { ids },
+        }),
+      });
+      const data = await response.json();
+      // console.log(data.result);
+
+      return data.result;
+    } catch (error) {
+      console.error("Error:", error);
+      return [];
+    }
+  },
+
+  getFields: async (field, offset, limit) => {
+    try {
+      const response = await fetch(API_URL, {
+        method: "POST",
+        ...authenticate(),
+        body: JSON.stringify({
+          action: "get_fields",
+          params: { field, offset, limit },
+        }),
+      });
+      const data = await response.json();
+      return data.result;
+    } catch (error) {
+      console.error("Error:", error);
+      return [];
+    }
+  },
+
+  filter: async (field, value) => {
+    try {
+      const response = await fetch(API_URL, {
+        method: "POST",
+        ...authenticate(),
+        body: JSON.stringify({
+          action: "filter",
+          params: { [field]: value },
+        }),
+      });
+      const data = await response.json();
+      return data.result;
+    } catch (error) {
+      console.error("Error:", error);
+      return [];
+    }
+  },
+};
+
+export default api;
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,25 +1,103 @@
 // src/components/ProductList.js
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import api from "../api";
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
+  const [uniqueIds, setUniqueIds] = useState([]);
+  const [repeatedIdsCount, setRepeatedIdsCount] = useState(0);
   const [page, setPage] = useState(1);
 
   useEffect(() => {
-    // Код для выполнения запроса к API и установки данных о товарах
+    // Запрос к API и установка данных о товарах
+    const fetchData = async () => {
+      try {
+        const itemsPerPage = 50;
+        const newUniqueIds = [];
+        let newRepeatedIdsCount = 0;
+        const allIds = await api.getAllIds();
+
+        if (
+          // page > uniqueIds / itemsPerPage &&
+          allIds.length > uniqueIds.length + repeatedIdsCount
+        ) {
+          // Выполняем цикл, пока не получим достаточное количество уникальных id для новой страницы
+          while (
+            newUniqueIds.length < itemsPerPage &&
+            allIds.length >
+              uniqueIds.length +
+                repeatedIdsCount +
+                newUniqueIds.length +
+                newRepeatedIdsCount
+          ) {
+            // Учитываем уже полученные уникальные идентификаторы и не прошедшие проверку в предыдущем цикле
+            const offset =
+              (page - 1) * itemsPerPage +
+              newUniqueIds.length +
+              repeatedIdsCount +
+              newRepeatedIdsCount;
+            const ids = await api.getIds(
+              offset,
+              itemsPerPage - newUniqueIds.length
+            );
+
+            console.log(allIds.length, offset, newUniqueIds.length, ids);
+
+            // Оставляем только уникальные идентификаторы
+            const uniqueIdsBatch = Array.from(new Set(ids)).filter(
+              (id) => !uniqueIds.includes(id)
+            );
+
+            // Подсчитываем количество повторяющихся id в текущей порции
+            newRepeatedIdsCount += itemsPerPage - uniqueIdsBatch.length;
+
+            // Добавляем уникальные идентификаторы текущей порции к общему списку
+            newUniqueIds.push(...uniqueIdsBatch);
+          }
+
+          console.log(uniqueIds, newUniqueIds);
+
+          // Обновляем состояние с уникальными идентификаторами
+
+            setUniqueIds((prevUniqueIds) => [
+              ...prevUniqueIds,
+              ...newUniqueIds,
+            ]);
+
+          // Обновляем состояние с количеством повторяющихся идентификаторов
+          setRepeatedIdsCount((prevCount) => prevCount + newRepeatedIdsCount);
+
+          // Получаем детали товаров только для уникальных идентификаторов текущей страницы
+          const detailedProducts = await api.getItems(newUniqueIds);
+          setProducts(detailedProducts);
+        }
+
+        console.log(
+          page,
+          uniqueIds,
+          newUniqueIds,
+          (page - 1) * itemsPerPage,
+          (page - 1) * itemsPerPage + itemsPerPage
+        );
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
   }, [page]);
 
   return (
     <div>
       <h1>Список товаров</h1>
       <ul>
-        {/* Отображение каждого товара в списке */}
+        {/* Отображаем каждый товар в списке */}
         {products.map((product) => (
           <li key={product.id}>
             <p>ID: {product.id}</p>
             <p>Название: {product.product}</p>
             <p>Цена: {product.price}</p>
-            <p>Бренд: {product.brand || 'Не указан'}</p>
+            <p>Бренд: {product.brand || "Не указан"}</p>
           </li>
         ))}
       </ul>
