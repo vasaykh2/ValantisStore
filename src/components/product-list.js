9ca8ffac1b05172bd72d60cd533f75ff59632fd6
Vadim Savinov <vasaykh@yandex.ru>
2024-03-07 16:11:29 +0500

    feat: наладить перелистывание вперед с сохранением id

--- src/components/product-list.js
+++ src/components/product-list.js
@@ -4,105 +4,133 @@ import api from "../api";
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
-  const [uniqueIds, setUniqueIds] = useState([]);
-  const [repeatedIdsCount, setRepeatedIdsCount] = useState(0);
+  const [ids, setIds] = useState({ uniqueIds: [[]], repeatedIdsCount: [0] });
   const [page, setPage] = useState(1);
+  const itemsPerPage = 50;
 
   useEffect(() => {
     // Запрос к API и установка данных о товарах
     const fetchData = async () => {
       try {
         const allIds = await api.getAllIds();
-        const itemsPerPage = 50;
-        const newUniqueIds = [];
+        let newUniqueIds = [];
         let newRepeatedIdsCount = 0;
         let uniqueDetailedProducts = [];
         // Учитываем уже полученные уникальные идентификаторы и не прошедшие проверку в предыдущем цикле
         let offset =
-        (page - 1) * itemsPerPage +
-        newUniqueIds.length +
-        repeatedIdsCount +
-        newRepeatedIdsCount;
+          (page - 1) * itemsPerPage +
+          newUniqueIds.length +
+          ids.repeatedIdsCount[page - 1] +
+          newRepeatedIdsCount;
 
         if (
-          // page > uniqueIds / itemsPerPage &&
-          allIds.length > uniqueIds.length + repeatedIdsCount
+                allIds.length >
+          ids.uniqueIds[page - 1].length + ids.repeatedIdsCount[page - 1]
         ) {
           // Выполняем цикл, пока не получим достаточное количество уникальных id для новой страницы
           while (
             newUniqueIds.length < itemsPerPage &&
             allIds.length >
-              uniqueIds.length +
-                repeatedIdsCount +
+            ids.uniqueIds[page - 1].length +
+              ids.repeatedIdsCount[page - 1] +
                 newUniqueIds.length +
                 newRepeatedIdsCount
           ) {
-
-            const ids = await api.getIds(
+            const newIds = await api.getIds(
               offset,
               itemsPerPage - newUniqueIds.length
             );
 
-            // console.log(allIds.length, offset, newUniqueIds.length, ids);
-
             // Оставляем только уникальные идентификаторы
-            const uniqueIdsBatch = Array.from(new Set(ids)).filter(
-              (id) => !uniqueIds.includes(id)
+            const uniqueIdsBatch = Array.from(new Set(newIds)).filter(
+              (id) => !ids.uniqueIds[page - 1].includes(id)
             );
-
-            // Подсчитываем количество повторяющихся id в текущей порции
-            newRepeatedIdsCount += itemsPerPage - newUniqueIds.length - uniqueIdsBatch.length;
 
             // Добавляем уникальные идентификаторы текущей порции к общему списку
             newUniqueIds.push(...uniqueIdsBatch);
+            // Подсчитываем количество повторяющихся id в текущей порции
+            newRepeatedIdsCount += itemsPerPage - newUniqueIds.length;
             // Получаем детали товаров только для уникальных идентификаторов текущей страницы
-           const detailedProducts = await api.getItems(newUniqueIds);
-           uniqueDetailedProducts = detailedProducts.filter(
-            (value, index, array) => {
-              return array.findIndex((obj) => obj.id === value.id) === index;
-            }
-          );
-          offset =
-        (page - 1) * itemsPerPage +
-        newUniqueIds.length +
-        repeatedIdsCount +
-        newRepeatedIdsCount;
+            const detailedProducts = await api.getItems(newUniqueIds);
+            // Дополнительно отфильтровываем только уникальные товары текущей страницы
+            uniqueDetailedProducts = detailedProducts.filter(
+              (value, index, array) => {
+                return array.findIndex((obj) => obj.id === value.id) === index;
+              }
+            );
+            offset =
+              (page - 1) * itemsPerPage +
+              newUniqueIds.length +
+              ids.repeatedIdsCount[page - 1] +
+              newRepeatedIdsCount;
           }
 
-          // console.log(uniqueIds, newUniqueIds);
 
-          // Обновляем состояние с уникальными идентификаторами
+          const arraysAreEqual = (arr1, arr2) => {
+            if (arr1.length !== arr2.length) {
+              return false;
+            }
+            for (let i = 0; i < arr1.length; i++) {
+              if (arr1[i] !== arr2[i]) {
+                return false;
+              }
+            }
+            return true;
+          }
+          // Обновляем состояние с уникальными идентификаторами и количеством повторяющихся идентификаторов
+          setIds((prevIds) => {
+            // Проверяем, чтобы избежать дублирования объектов
+            const lastUniqueIds =
+              prevIds.uniqueIds[prevIds.uniqueIds.length - 1];
+            if (!arraysAreEqual(lastUniqueIds, newUniqueIds)) {
+              const updatedUniqueIds = [...prevIds.uniqueIds, newUniqueIds];
+              const updatedRepeatedIdsCount = [
+                ...prevIds.repeatedIdsCount,
+                prevIds.repeatedIdsCount[prevIds.repeatedIdsCount.length - 1] +
+                  newRepeatedIdsCount,
+              ];
+              // Возвращаем обновленное состояние
+              return {
+                uniqueIds: updatedUniqueIds,
+                repeatedIdsCount: updatedRepeatedIdsCount,
+              };
+            }
+            // Возвращаем предыдущее состояние, если новые данные совпадают с последними
+            return prevIds;
+          });
 
-            setUniqueIds((prevUniqueIds) => [
-              ...prevUniqueIds,
-              ...newUniqueIds,
-            ]);
-
-          // Обновляем состояние с количеством повторяющихся идентификаторов
-          setRepeatedIdsCount((prevCount) => prevCount + newRepeatedIdsCount);
-
-          // Получаем детали товаров только для уникальных идентификаторов текущей страницы
-          // const detailedProducts = await api.getItems(newUniqueIds);
           setProducts(uniqueDetailedProducts);
         }
 
         console.log(
-          `страница -> ${page}  offset -> ${offset}`
+          `страница -> ${page} length -> ${ids.repeatedIdsCount.length} offset -> ${offset} newUniqueIds.length -> ${newUniqueIds.length} repeatedIdsCount -> ${ids.repeatedIdsCount[page - 1]} newRepeatedIdsCount -> ${newRepeatedIdsCount}`
         );
-        console.log(
-          `uniqueDetailedProducts -> ${uniqueDetailedProducts.length}: ${JSON.stringify(
-            uniqueDetailedProducts[0],
-            null,
-            2
-          )} <> ${JSON.stringify(uniqueDetailedProducts[uniqueDetailedProducts.length - 1], null, 2)}`
-        );
-        console.log(
-            `products -> ${products.length}: ${JSON.stringify(
-              products[0],
-              null,
-              2
-            )} <> ${JSON.stringify(products[products.length - 1], null, 2)}`
-          );
+        // console.log(
+        //   `uniqueDetailedProducts -> ${
+        //     uniqueDetailedProducts.length
+        //   }: ${JSON.stringify(
+        //     uniqueDetailedProducts[0],
+        //     null,
+        //     2
+        //   )} <> ${JSON.stringify(
+        //     uniqueDetailedProducts[uniqueDetailedProducts.length - 1],
+        //     null,
+        //     2
+        //   )}`
+        // );
+        // console.log(
+        //   `products -> ${products.length}: ${JSON.stringify(
+        //     products[0],
+        //     null,
+        //     2
+        //   )} <> ${JSON.stringify(products[products.length - 1], null, 2)}`
+        // );
+        // console.log(
+        //   `ids -> ${JSON.stringify(ids, null, 2)}`,
+        //   uniqueIds,
+        //   repeatedIdsCount
+        // );
+        // console.log(`ids -> ${ids.uniqueIds[page - 1]}, ${ids.repeatedIdsCount[page - 1]}`, uniqueIds, repeatedIdsCount);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -110,7 +138,6 @@ const ProductList = () => {
 
     fetchData();
   }, [page]);
-
 
   return (
     <div>
